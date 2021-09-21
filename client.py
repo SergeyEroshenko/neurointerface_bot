6d5ded0cd12a8dec2ac409b35b75127e050be5aa
Sergey Eroshenko <sergey.eroshenko.nsk@gmail.com>
2021-09-21 13:38:58 +0300

    add /stop command

--- client.py
+++ client.py
@@ -9,28 +9,18 @@ from security_data import CLIENT_ID, CLIENT_HASH
 from settings import BOT_NAME
 
 
-def get_env(name, message, cast=str):
-    if name in os.environ:
-        return os.environ[name]
-    while True:
-        value = input(message)
-        try:
-            return cast(value)
-        except ValueError as e:
-            print(e, file=sys.stderr)
-            time.sleep(1)
-
-
 session = os.environ.get('TG_SESSION', 'neurointerface')
-api_id = CLIENT_ID # get_env('TG_API_ID', 'Enter your API ID: ', int)
-api_hash = CLIENT_HASH # get_env('TG_API_HASH', 'Enter your API hash: ')
+api_id = CLIENT_ID
+api_hash = CLIENT_HASH
 proxy = None 
 buttons = list(range(4))
 client = TelegramClient(session, api_id, api_hash, proxy=proxy).start()
 
 
-@client.on(events.NewMessage(from_users=BOT_NAME))
+@client.on(events.NewMessage(from_users=[BOT_NAME, 'me']))
 async def handler(event):
+    if event.message.message == "/stop":
+        await client.disconnect()
     await asyncio.sleep(3)
     await event.click(random.choice(buttons))
 
