c05fcd020fa5f2b985ccd2883ac3258c3f5e5645
Лисин Федор Альбертович <fedor.lisin@ocrv.ru>
2021-12-07 14:26:32 +0300

    update Subject logic

--- neurointerface_lib/core/observer.py
+++ neurointerface_lib/core/observer.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
+from typing import List
 
 
 class Observer(ABC):
@@ -7,38 +9,18 @@ class Observer(ABC):
     используют для оповещения своих подписчиков.
     """
     @abstractmethod
-    def update(self, subject):
+    def update(self, subject: Subject):
         """
         Получить обновление от субъекта.
         """
-        pass
+        raise NotImplementedError
 
 
 class Subject(ABC):
 
-    @abstractmethod
-    def attach(self, observer):
-        """
-        Присоединяет наблюдателя к издателю.
-        """
-        pass
-
-    @abstractmethod
-    def detach(self, observer):
-        """
-        Отсоединяет наблюдателя от издателя.
-        """
-        pass
-
-    @abstractmethod
-    def notify(self):
-        """
-        Уведомляет всех наблюдателей о событии.
-        """
-        pass
-
-
-class ConcreteSubject(Subject):
+    def __init__(self) -> None:
+        self._observers: List[Observer]
+        self._state: str
 
     def attach(self, observer: Observer):
         self._observers.append(observer)
@@ -52,3 +34,40 @@ class ConcreteSubject(Subject):
                 "Subject without Observers. Attach, please.")
         for observer in self._observers:
             observer.update(self)
+
+    # @abstractmethod
+    # def attach(self, observer: Observer):
+    #     """
+    #     Присоединяет наблюдателя к издателю.
+    #     """
+    #     raise NotImplementedError
+
+    # @abstractmethod
+    # def detach(self, observer: Observer):
+    #     """
+    #     Отсоединяет наблюдателя от издателя.
+    #     """
+    #     raise NotImplementedError
+
+    # @abstractmethod
+    # def notify(self):
+    #     """
+    #     Уведомляет всех наблюдателей о событии.
+    #     """
+    #     raise NotImplementedError
+
+
+# class ConcreteSubject(Subject):
+
+#     def attach(self, observer: Observer):
+#         self._observers.append(observer)
+
+#     def detach(self, observer: Observer):
+#         self._observers.remove(observer)
+
+#     def notify(self):
+#         if self._observers is None:
+#             raise Exception(
+#                 "Subject without Observers. Attach, please.")
+#         for observer in self._observers:
+#             observer.update(self)
