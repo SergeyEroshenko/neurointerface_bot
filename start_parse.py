178d339bfa729f387c68f6b37d53adf05ff70be4
Лисин Федор Альбертович <fedor.lisin@ocrv.ru>
2021-12-17 16:33:55 +0300

    replace inspector by timeout, add parser to conn

--- start_parse.py
+++ start_parse.py
@@ -7,6 +7,7 @@ from neurointerface_lib.parser import Parser, DBWriter
 from db_settings import db_conn_params
 from clickhouse_driver import Client
 from start_args_parse import args_parser
+from async_timeout import timeout
 
 
 def timer_beeper(t: int = 3):
@@ -17,48 +18,69 @@ def timer_beeper(t: int = 3):
     beep(3)
 
 
-async def inspector(time: int):
+async def inspector(time: int, conn):
     await asyncio.sleep(time)
-    print("close all coro ")
-    # for task in asyncio.all_tasks():
-    #     print(task)
-    tasks = asyncio.all_tasks()
-    for task in tasks:
-        task_name = task.get_name()
-        coro_type = str(task.get_coro())
-        if ("main()" not in coro_type) | ("inspector()" not in coro_type):
-            # task.cancel()
-            print(task_name, " is closed. ", len(list(asyncio.all_tasks())))
-        await asyncio.sleep(0.01)
-    list(asyncio.all_tasks())[0].cancel()
+    await conn.disable_data_grab_mode()
+    await conn.disconnect()
+    # print("close all coro ")
+    # # for task in asyncio.all_tasks():
+    # #     print(task)
+    # tasks = asyncio.all_tasks()
+    # for task in tasks:
+    #     task.cancel()
+    # raise asyncio.TimeoutError
+    #     task_name = task.get_name()
+    #     coro_type = str(task.get_coro())
+    #     if ("main()" not in coro_type) | ("inspector()" not in coro_type):
+    #         # task.cancel()
+    #         print(task_name, " is closed. ", len(list(asyncio.all_tasks())))
+    #     await asyncio.sleep(0.01)
+    # list(asyncio.all_tasks())[0].cancel()
 
 
 async def main(human_id, label):
-    conn = Connector(device_id="0446", freq=5)
     parser = Parser()
+    conn = Connector(device_id="0446", parser=parser)
     db_conn = Client(database=f'human_{human_id}', **db_conn_params)
     db_writer = DBWriter(db_conn, label)
 
-    conn.attach(db_writer)
-    conn.attach(parser)
+    parser.attach(db_writer)
+    # parser.attach(parser)
     await conn.connect()
     await conn.enable_data_grab_mode()
+
+    await conn.set_data_storage_time("1")
+
+    from datetime import datetime
+
+    now = datetime.now().time()
+    print(now)
     tasks = [
         conn.handler(),
-        conn.rhythms_history(),
-        # conn.rhythms(),
-        inspector(time=10)
+        # conn.rhythms_history(),
+        conn.rhythms(),
+        conn.grab_raw_data(),
+        # inspector(time=10, conn=conn)
     ]
-    await asyncio.gather(*tasks)
+
+    # await asyncio.gather(*tasks)
+    try:
+        async with timeout(10):
+            await asyncio.gather(*tasks)
+    except asyncio.TimeoutError:
+        print("Task finished!")
+        beep(3)
+        await conn.disable_data_grab_mode()
+        await conn.disconnect()
 
 
 if __name__ == '__main__':
     human_id, label = args_parser()
     time.sleep(3)
-    # client = Client(**db_conn_params)
+    client = Client(**db_conn_params)
     database_name = f"human_{human_id}"
-    # client.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
-    # client.disconnect()
+    client.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+    client.disconnect()
     print(f"Database name is: {database_name}\nAction label is: {label}")
-    timer_beeper(5)
-    # asyncio.run(main(human_id, label))
+    timer_beeper(3)
+    asyncio.run(main(human_id, label))
